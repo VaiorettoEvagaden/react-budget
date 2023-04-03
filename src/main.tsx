3d1a01dcceb67ad3a36f4cda9e555f1711d3038b
VaiorettoEvagaden <cscfgogogo@gmail.com>
2023-04-03 22:59:56 +0800

    change basic config

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,30 @@
-import App from 'App'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { registerSW } from 'virtual:pwa-register'
-import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import App from 'App';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { registerSW } from 'virtual:pwa-register';
+import './index.css';
 
-registerSW()
+registerSW();
 
-const MAX_RETRIES = 1
+const MAX_RETRIES = 1;
 const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			staleTime: Number.POSITIVE_INFINITY,
-			retry: MAX_RETRIES
-		}
-	}
-})
+  defaultOptions: {
+    queries: {
+      staleTime: Number.POSITIVE_INFINITY,
+      retry: MAX_RETRIES
+    }
+  }
+});
 
-const container = document.querySelector('#root')
+const container = document.querySelector('#root');
 if (container) {
-	const root = createRoot(container)
-	root.render(
-		<StrictMode>
-			<QueryClientProvider client={queryClient}>
-				<App />
-			</QueryClientProvider>
-		</StrictMode>
-	)
+  const root = createRoot(container);
+  root.render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </StrictMode>
+  );
 }
