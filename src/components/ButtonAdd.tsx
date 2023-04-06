71d2e72bf76828c682de6df8949353fa36e426d8
VaiorettoEvagaden <cscfgogogo@gmail.com>
2023-04-06 22:33:16 +0800

    test

--- src/components/ButtonAdd.tsx
+++ src/components/ButtonAdd.tsx
@@ -2,11 +2,14 @@ import type { ReactElement } from 'react';
 import IconSvg from './IconSvg';
 
 // interface ButtonAddProperties {}
-
+function onClick(parameters: React.MouseEvent): void {
+  console.log(parameters);
+}
 function ButtonAdd(): ReactElement {
   return (
     <button
       type='button'
+      onClick={onClick}
       className='fixed bottom-24 right-4 flex  h-16 w-16 items-center justify-center rounded-full bg-violet-400 shadow-lg'
     >
       <IconSvg iconName='add' iconColorClass='fill-white w-8 h-8' />
